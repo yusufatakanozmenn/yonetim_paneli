f6a51bf07ab0d7b2f6febd49fa514744e3eb81ab
Onur Dogu <onur.altinisik@alvagrup.com>
2023-02-24 15:19:20 +0300

    Customer Management Pages Added

---
+++ panel/application/controllers/Notification_Templates.php
@@ -0,0 +1,20 @@
+<?php
+class Notification_Templates extends CI_Controller {
+
+	public $viewFolder = "";
+	public function __construct()
+	{
+		parent::__construct();
+		$this->viewFolder = "notification_templates_v";
+	}
+    public function index(){
+        $viewData = new stdClass();
+        $viewData->viewFolder=$this->viewFolder;
+        $viewData->subViewFolder="update";
+
+        $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index",$viewData);
+    }
+
+
+
+}
---
+++ panel/application/views/send_bulk_sms_v/update/content.php
@@ -0,0 +1,1 @@
+Toplu SMS Gonder
