4413522799084d83930c6ea41ba110c34f0870df
Helin <65095387+HelinUcar@users.noreply.github.com>
2023-02-24 15:21:52 +0300

    list sayfası

--- panel/application/controllers/Product.php
+++ panel/application/controllers/Product.php
@@ -6,11 +6,16 @@ class Product extends CI_Controller {
 	{
 		parent::__construct();
 		$this->viewFolder = "product_v";
+        $this->load->model("Product_model");
 	}
     public function index(){
         $viewData = new stdClass();
+        /** Tablodaki tüm kayıtları getiriyoruz.. */
+        $items = $this->Product_model->get_all();
+        /** View'e gönderilecek değişkenler.. */
         $viewData->viewFolder=$this->viewFolder;
         $viewData->subViewFolder="list";
+        $viewData->items=$items;
 
         $this->load->view("{$this->viewFolder}/{$viewData->subViewFolder}/index",$viewData);
     }
