0fefb6f665a7f253eb920b903b53e5c418db5450
Yusuf Atakan Özmen <92385960+yusufatakanozmenn@users.noreply.github.com>
2023-02-24 16:01:16 +0300

    Update content.php

--- panel/application/views/product_v/add/content.php
+++ panel/application/views/product_v/add/content.php
@@ -1,26 +1,52 @@
 <div class="row">
-    <div class="col-md-12">
-        <h4 class="m-b-lg">
-            Yeni Ürün Ekle
-        </h4>
-    </div><!-- END column -->
-    <div class="col-md-12">
-        <div class="widget">
-            <div class="widget-body">
-                <form action="<?php echo base_url("product/save"); ?>" method="post">
-                    <div class="form-group">
-                        <label>Başlık</label>
-                        <input class="form-control" placeholder="Başlık" name="title">
+    <div class="col-12 grid-margin stretch-card">
+        <div class="card">
+            <div class="card-body">
+                <form class="forms-sample" method="post" action="../_class/yonetim_islem.php"
+                    enctype="multipart/form-data">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="site_url">Site Açılış Tarihi</label>
+                                <div id="datepicker-popup" class="input-group date datepicker">
+                                    <input type="text" class="form-control" name="acilis_tarih" value="26-09-2022">
+                                    <span class="input-group-addon input-group-append border-left">
+                                        <span class="mdi mdi-calendar input-group-text"></span>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="site_url">Site Açılış Zamanı</label>
+                                <div class="input-group date" id="timepicker-example" data-target-input="nearest">
+                                    <div class="input-group" data-target="#timepicker-example"
+                                        data-toggle="datetimepicker">
+                                        <input type="text" name="acilis_zaman" class="form-control datetimepicker-input"
+                                            value="10:10" data-target="#timepicker-example">
+                                        <div class="input-group-addon input-group-append"><i
+                                                class="mdi mdi-clock input-group-text"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div class="form-group">
-                        <label>Açıklama</label>
-                        <textarea name="description" class="m-0" data-plugin="summernote"
-                            data-options="{height: 250}"></textarea>
+                        <label for="baslik">Başlık</label>
+                        <input type="text" class="form-control form-control-sm" name="baslik" id="baslik"
+                            value="SİTEMİZ BAKIMDADIR">
                     </div>
-                    <button type="submit" class="btn btn-primary btn-md btn-outline">Kaydet</button>
-                    <a href="<?php echo base_url("product"); ?>" class="btn btn-md btn-danger btn-outline">İptal</a>
+                    <div class="form-group">
+                        <label for="aciklama">Açıklama</label>
+                        <textarea id="aciklama" name="aciklama" class="form-control"
+                            rows="4">Şu anda bakımdayız. Kısa süre sonra geri döneceğiz .Daha sonra yeniden deneyiniz.</textarea>
+                    </div>
+                    <button type="submit" name="site_bakim_modu" class="btn btn-success btn-icon-text btn-sm">
+                        <i class="mdi mdi-spin mdi-loading"></i>
+                        GÜNCELLE
+                    </button>
                 </form>
-            </div><!-- .widget-body -->
-        </div><!-- .widget -->
-    </div><!-- END column -->
+            </div>
+        </div>
+    </div>
 </div>
