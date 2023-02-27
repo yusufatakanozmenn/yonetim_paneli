13f0e81236ce5fe50a0157fb370998b22ec795ef
Yusuf Atakan Özmen <92385960+yusufatakanozmenn@users.noreply.github.com>
2023-02-27 15:02:21 +0300

    genel ayarlar tam çalışmıyor

--- panel/application/controllers/General_Settings.php
+++ panel/application/controllers/General_Settings.php
@@ -13,14 +13,128 @@ class General_Settings extends CI_Controller
     {
         $viewData = new stdClass();
 
+        /** Tablodan Verilerin Getirilmesi.. */
+        $item = $this->General_Settings_model->get();
+
+        if($item)
+            $viewData->subViewFolder = "update";
+        else
+            $viewData->subViewFolder = "no_content";
+
+        /** View'e gönderilecek Değişkenlerin Set Edilmesi.. */
         $viewData->viewFolder = $this->viewFolder;
-        $viewData->subViewFolder = "update";
+        $viewData->item = $item;
+
+        $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+    }
+    public function new_form(){
+
+        $viewData = new stdClass();
+
+        /** View'e gönderilecek Değişkenlerin Set Edilmesi.. */
+        $viewData->viewFolder = $this->viewFolder;
+        $viewData->subViewFolder = "add";
+
+        $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+
+    }
+    public function save(){
+
+            $this->load->library("form_validation");
+
+            // Kurallar yazilir..
+            $this->form_validation->set_rules("site_url", "site_url", "required|trim");
+            $this->form_validation->set_rules("site_title", "site_title", "required|trim");
+            $this->form_validation->set_rules("site_description", "site_description", "required|trim");
+            $this->form_validation->set_rules("site_keywords", "site_keywords", "required|trim");
+            $this->form_validation->set_rules("pbirimi", "pbirimi", "required|trim");
+            $this->form_validation->set_rules("video", "video", "required|trim");
+            $this->form_validation->set_rules("yonetim", "yonetim", "required|trim");
+            $this->form_validation->set_rules("site_desc", "site_desc", "required|trim");
+            $this->form_validation->set_rules("copyright", "copyright", "required|trim");
+
+        // Form Validation Calistirilir..
+        $validate = $this->form_validation->run();
+
+        if($validate){
+            $insert = $this->General_Settings_model->add(
+                array(
+                    "site_url"          => $this->input->post("site_url"),
+                    "site_title"        => $this->input->post("site_title"),
+                    "site_description"  => $this->input->post("site_description"),
+                    "site_keywords"     => $this->input->post("site_keywords"),
+                    "pbirimi"           => $this->input->post("pbirimi"),
+                    "video"             => $this->input->post("video"),
+                    "yonetim"           => $this->input->post("yonetim"),
+                    "site_desc"         => $this->input->post("site_desc"),
+
+            )
+        );
 
 
-        $this->load->view("{$this->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+            // TODO Alert sistemi eklenecek...
+            if($insert){
+
+                $alert = array(
+                    "title" => "İşlem Başarılı",
+                    "text" => "Kayıt başarılı bir şekilde eklendi",
+                    "type"  => "success"
+                );
+
+            } else {
+
+                $alert = array(
+                    "title" => "İşlem Başarısız",
+                    "text" => "Kayıt Ekleme sırasında bir problem oluştu",
+                    "type"  => "error"
+                );
+            }
+
+
+            $this->session->set_flashdata("alert", $alert);
+
+            redirect(base_url("settings"));
+
+            die();
+
+        } else {
+
+            $viewData = new stdClass();
+
+            /** View'e gönderilecek Değişkenlerin Set Edilmesi.. */
+            $viewData->viewFolder = $this->viewFolder;
+            $viewData->subViewFolder = "add";
+            $viewData->form_error = true;
+
+            $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+        }
+
     }
 
-    
+    public function update_form($id){
+
+        $viewData = new stdClass();
+
+        /** Tablodan Verilerin Getirilmesi.. */
+        $item = $this->General_Settings_model->get(
+            array(
+                "id"    => $id,
+            )
+        );
+
+        /** View'e gönderilecek Değişkenlerin Set Edilmesi.. */
+        $viewData->viewFolder = $this->viewFolder;
+        $viewData->subViewFolder = "update";
+        $viewData->item = $item;
+
+        $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+
+
+    }
+
+
+
+
 
 
 
--- panel/application/views/general_settings_v/update/content.php
+++ panel/application/views/general_settings_v/update/content.php
@@ -14,141 +14,129 @@
                             <div class="col-12 grid-margin stretch-card">
                                 <div class="card">
                                     <div class="card-body">
-                                        <form class="forms-sample" method="post" action="../_class/yonetim_islem.php"
+                                        <form class="forms-sample" method="post" action="<?php echo base_url("panel/general_settings/update");?>
                                             enctype="multipart/form-data">
-                                            <div class="row">
-                                                <div class="col-md-4">
-                                                    <div class="card mb-3">
-                                                        <div class="card-header">
-                                                            Logo Yükle
-                                                        </div>
-                                                        <div class="card-body">
-                                                            <div class="form-group">
-                                                                <input type="file" name="resim"
-                                                                    class="file-upload-default">
-                                                                <div class="input-group col-xs-12">
-                                                                    <div class="col-md-6 col-sm-6">
-                                                                        <a href="#" class="thumbnail white">
-                                                                            <img src="<?php echo base_url("assets");?>/assets/images/alva_favicon.svg"
-                                                                                alt="...">
-                                                                        </a>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-4">
+                                                <div class="card mb-3">
+                                                    <div class="card-header">
+                                                        Logo Yükle
                                                     </div>
-                                                </div>
-                                                <div class="col-md-4">
-                                                    <div class="card mb-3">
-                                                        <div class="card-header">
-                                                            Mail Şablonu ve Footer logo yükle
-                                                        </div>
-                                                        <div class="card-body">
-                                                            <div class="form-group">
-                                                                <input type="file" name="footer"
-                                                                    class="file-upload-default">
-                                                                <div class="input-group col-xs-12">
-                                                                    <div class="col-md-6 col-sm-6">
-                                                                        <a href="#" class="thumbnail white">
-                                                                            <img src="<?php echo base_url("assets");?>/assets/images/alva_favicon.svg"
-                                                                                alt="...">
-                                                                        </a>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="col-md-4">
-                                                    <div class="card mb-3">
-                                                        <div class="card-header">
-                                                            Favicon Yükle
-                                                        </div>
-                                                        <div class="card-body">
-                                                            <div class="form-group">
-                                                                <input type="file" name="favicon"
-                                                                    class="file-upload-default">
-                                                                <div class="input-group col-xs-12">
-                                                                    <div class="col-md-6 col-sm-6">
-                                                                        <a href="#" class="thumbnail white">
-                                                                            <img src="<?php echo base_url("assets");?>/assets/images/alva_favicon.svg"
-                                                                                alt="...">
-                                                                        </a>
-                                                                    </div>
+                                                    <div class="card-body">
+                                                        <div class="form-group">
+                                                            <input type="file" name="firma_logo"
+                                                                   class="file-upload-default">
+                                                            <div class="input-group col-xs-12">
+                                                                <div class="col-md-6 col-sm-6">
+                                                                    <a href="#" class="thumbnail white">
+                                                                        <img src="<?php echo base_url("assets");?>assets"<?php $item->firma_logo;?>"
+                                                                        alt="...">
+                                                                    </a>
                                                                 </div>
                                                             </div>
                                                         </div>
                                                     </div>
                                                 </div>
                                             </div>
-                                            <div class="form-group">
-                                                <label for="site_url">Site Url</label>
-                                                <input type="text" class="form-control form-control-sm" name="site_url"
-                                                    id="site_url" value="">
-                                            </div>
-                                            <div class="form-group">
-                                                <label for="site_title">Site Title</label>
-                                                <input type="text" class="form-control form-control-sm"
-                                                    name="site_title" id="site_title" value="">
-                                            </div>
-                                            <div class="form-group">
-                                                <label for="pbirim">Para Birimi</label>
-                                                <select class="form-control form-control-sm" name="pbirim">
-                                                    <option selected="">TL</option>
-                                                    <option>USD</option>
-                                                    <option>EURO</option>
-                                                </select>
-                                            </div>
-                                            <div class="form-group">
-                                                <label for="video">Firma Tanıtım Video Url Linki
-                                                    <small></small></label><small>
-                                                    <input type="text" class="form-control form-control-sm" name="video"
-                                                        id="video" value="">
-                                                </small>
-                                            </div><small>
-                                                <div class="form-group">
-                                                    <label for="site_url"><b>Yönetim Paneli Girişi Link Adresi (Örnek:
-                                                            xyonetim, ypanel,
-                                                            yadminx, Not: TR Karekter Kullanmayınız.!)</b></label>
-                                                    <input type="text" class="form-control form-control-sm"
-                                                        name="yonetim" id="yonetim" value="">
+                                            <div class="col-md-4">
+                                                <div class="card mb-3">
+                                                    <div class="card-header">
+                                                        Mail Şablonu ve Footer logo yükle
+                                                    </div>
+                                                    <div class="card-body">
+                                                        <div class="form-group">
+                                                            <input type="file" name="firma_footerlogo"
+                                                                   class="file-upload-default">
+                                                            <div class="input-group col-xs-12">
+                                                                <div class="col-md-6 col-sm-6">
+                                                                    <a href="#" class="thumbnail white">
+                                                                        <img src="<?php echo base_url("assets");?>assets"<?php $item->firma_footerlogo;?>"
+                                                                        alt="...">
+                                                                    </a>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
                                                 </div>
-
-                                                <div class="form-group">
-                                                    <label for="maxlength-textarea">SEO Açıklama (Description)</label>
-                                                    <textarea id="maxlength-textarea" name="site_desc"
-                                                        class="form-control" maxlength="260" rows="4"></textarea>
+                                            </div>
+                                            <div class="col-md-4">
+                                                <div class="card mb-3">
+                                                    <div class="card-header">
+                                                        Favicon Yükle
+                                                    </div>
+                                                    <div class="card-body">
+                                                        <div class="form-group">
+                                                            <input type="file" name="favicon"
+                                                                   class="file-upload-default">
+                                                            <div class="input-group col-xs-12">
+                                                                <div class="col-md-6 col-sm-6">
+                                                                    <a href="#" class="thumbnail white">
+                                                                        <img src="<?php echo base_url("assets");?>assets"<?php $item->favicon;?>"                                                                                alt="...">
+                                                                    </a>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
                                                 </div>
-                                                <div class="form-group">
-                                                    <label for="copyright">Copyright Metni</label>
-                                                    <input type="text" class="form-control form-control-sm"
-                                                        name="copyright" id="copyright" value="">
-                                                </div>
-                                                <button type="submit" name="genel_ayarlar"
-                                                    class="btn btn-success btn-icon-text btn-sm">
-                                                    <i class="mdi mdi-spin mdi-loading"></i>
-                                                    GÜNCELLE
-                                                </button>
-
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="site_url">Site Url</label>
+                                            <input type="text" class="form-control form-control-sm" name="site_url"
+                                                   id="site_url" value="">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="site_title">Site Title</label>
+                                            <input type="text" class="form-control form-control-sm"
+                                                   name="site_baslik" id="site_baslik" value="">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="pbirim">Para Birimi</label>
+                                            <select class="form-control form-control-sm" name="pbirim">
+                                                <option selected="">TL</option>
+                                                <option>USD</option>
+                                                <option>EURO</option>
+                                            </select>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="video">Firma Tanıtım Video Url Linki
+                                                <small></small></label><small>
+                                                <input type="text" class="form-control form-control-sm" name="video"
+                                                       id="video" value="">
                                             </small>
+                                        </div><small>
+                                            <div class="form-group">
+                                                <label for="site_url"><b>Yönetim Paneli Girişi Link Adresi (Örnek:
+                                                        xyonetim, ypanel,
+                                                        yadminx, Not: TR Karekter Kullanmayınız.!)</b></label>
+                                                <input type="text" class="form-control form-control-sm"
+                                                       name="yonetim" id="yonetim" value="">
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label for="maxlength-textarea">SEO Açıklama (Description)</label>
+                                                <textarea id="maxlength-textarea" name="site_desc"
+                                                          class="form-control" maxlength="260" rows="4"></textarea>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="copyright">Copyright Metni</label>
+                                                <input type="text" class="form-control form-control-sm"
+                                                       name="copyright" id="copyright" value="">
+                                            </div>
+                                            <button type="submit" name="genel_ayarlar"
+                                                    class="btn btn-success btn-icon-text btn-sm">
+                                                <i class="mdi mdi-spin mdi-loading"></i>
+                                                GÜNCELLE
+                                            </button>
+
+                                        </small>
                                         </form>
-                                    </div><small>
-                                    </small>
-                                </div><small>
-                                </small>
-                            </div><small>
-
-                            </small>
-                        </div><small>
-
-                        </small>
-                    </div><small>
-                        <!-- content-wrapper ends -->
-
-                        <!-- partial -->
-                    </small>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div><!-- END column -->
-        </div>
+
     </section><!-- #dash-content -->
-</div>
+</div><!-- .wrap -->
