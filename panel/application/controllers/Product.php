64c7b92f2fe3a5aeb2ddb886068b2204caef16c9
Yusuf Atakan Özmen <92385960+yusufatakanozmenn@users.noreply.github.com>
2023-02-27 15:14:03 +0300

    ürün silme eklendi

--- panel/application/controllers/Product.php
+++ panel/application/controllers/Product.php
@@ -84,6 +84,19 @@ class Product extends CI_Controller
 
 
     }
+    public function delete($id){
+        $delete = $this->product_model->delete(
+            array(
+                "id" => $id
+            )
+        );
+        if($delete){
+            redirect(base_url("product"));
+        }
+        else{
+            echo "Silme İşlemi Gerçekleşmedi";
+        }
+    }
 
 
 }
--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -81,9 +81,10 @@
                             data-switchery 
                             data-color="#10c469"
                             <?php echo ($item->durum) ? "checked" : "";  ?>/>
+
                         </td>
                         <td>
-                            <a href="#" class="btn btn-sm btn-danger btn-outline"><i class="fa fa-trash"></i>
+                            <a href="<?php echo base_url("product/delete/$item->id");?>" class="btn btn-sm btn-danger btn-outline"><i class="fa fa-trash"></i>
                                 Sil</a>
                             <a href="<?php echo base_url("product/update_form/$item->id");?>" class="btn btn-sm btn-info btn-outline"><i class="fa fa-pencil-square-o"></i>
                                 Düzenle</a>
