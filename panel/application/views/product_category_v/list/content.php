8983921af28a63adf8609eb5eaca89e46054ca84
Yusuf Atakan Özmen <92385960+yusufatakanozmenn@users.noreply.github.com>
2023-03-01 11:25:08 +0300

    proje yönetimi

--- panel/application/views/product_category_v/list/content.php
+++ panel/application/views/product_category_v/list/content.php
@@ -1,179 +1,78 @@
 <div class="row">
     <div class="col-md-12">
         <h4 class="m-b-lg">
-            Ürün Kategorileri Listesi
+            Ürün Kategori Listesi
         </h4>
         <h4>
 
-            <a href="http://localhost/yonetim_paneli/panel/product/new_form" class="btn btn-outline btn-primary btn-sm">
-
+            <a href="<?php echo base_url("product_category"); ?>" class="btn btn-outline btn-primary btn-sm">
                 <i class="fa fa-plus-circle"></i> Yeni Ekle</a>
-            <a href="javascript:void(0)" class="dropdown-toggle btn btn-outline btn-primary btn-sm" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="fa fa-ellipsis-v"></i> Seçilenlere Uygula </a>
-            <div class="media-group dropdown-menu animated flipInY" style="margin-left:5rem; ">
-                <li><a href="javascript:void(0)"><i class="zmdi m-r-md zmdi-hc-lg zmdi-account-box"></i>My
-                        Profile</a></li>
-                <li><a href="javascript:void(0)"><i class="zmdi m-r-md zmdi-hc-lg zmdi-balance-wallet"></i>Balance</a>
-                </li>
-                <li><a href="javascript:void(0)"><i class="zmdi m-r-md zmdi-hc-lg zmdi-info"></i>privacy</a>
-                </li>
-            </div>
-            <a href="javascript:void(0)" class="dropdown-toggle btn btn-outline btn-warning btn-sm" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="fa fa-ellipsis-v"></i> Excel İşlemleri </a>
-            <div class="media-group dropdown-menu animated flipInY" style="margin-left:13rem; ">
-                <li><a href="javascript:void(0)"><i class="zmdi m-r-md zmdi-hc-lg zmdi-account-box"></i>My
-                        Profile</a></li>
-                <li><a href="javascript:void(0)"><i class="zmdi m-r-md zmdi-hc-lg zmdi-balance-wallet"></i>Balance</a>
-                </li>
-                <li><a href="javascript:void(0)"><i class="zmdi m-r-md zmdi-hc-lg zmdi-info"></i>privacy</a>
-                </li>
-            </div>
-            <a href="#" class="btn btn-outline btn-danger btn-sm">
-                <i class="fa fa-trash"></i> Tüm Veriyi Sil</a>
+            <!-- <a href="#" class="btn btn-outline btn-danger btn-sm">
+                <i class="fa fa-trash"></i> Tüm Veriyi Sil</a> -->
         </h4>
     </div><!-- END column -->
     <div class="col-md-12">
         <div class="widget p-lg">
 
-            
+            <?php
+            if (empty($items)) { ?>
+            <div class="alert alert-info text-center">
+                <p>Burada herhangi bir veri bulunmamaktadır. Eklemek için lütfen <a
+                        href="<?php echo base_url("product/new_form"); ?>">tıklayınız</a></p>
+            </div>
+            <?php } else { ?>
+
             <table class="table table-hover table-striped">
                 <thead>
-                    <tr><th>
+                    <th>
                         <div class="checkbox checkbox-primary">
-                            <input type="checkbox" id="checkbox-demo-1">
+                            <input type="checkbox" id="checkbox-demo-1" />
                             <label for="checkbox-demo-1"></label>
                         </div>
                     </th>
-                    <th>Sıra</th>
+
                     <th>Başlık</th>
-                    <th>Ürün Kodu</th>
-                    <th>Fiyat</th>
-                    <th>Açıklama</th>
+                    <th>SEF Url</th>
                     <th>Durumu</th>
                     <th>İşlem</th>
-                </tr></thead>
+                </thead>
                 <tbody>
-                                            <tr>
+                    <!-- <?php foreach ($items as $item) { ?>
+                    <tr>
                         <td>
                             <div class="checkbox checkbox-primary">
-                                <input type="checkbox" id="checkbox-demo-1">
+                                <input type="checkbox" id="checkbox-demo-1" />
                                 <label for="checkbox-demo-1"></label>
                             </div>
                         </td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td><?php echo $item->id;?></td>
+                        <td><?php echo $item->adi;?></td>
+                        <td><?php echo $item->urun_kodu;?></td>
+                        <td><?php echo $item->fiyat;?></td>
+                        <td><?php echo $item->aciklama;?></td>
                         <td>
-                            <input id="switch-2-2" type="checkbox" data-switchery="true" data-color="#10c469" style="display: none;"><span class="switchery switchery-default" style="box-shadow: rgb(223, 223, 223) 0px 0px 0px 0px inset; border-color: rgb(223, 223, 223); background-color: rgb(255, 255, 255); transition: border 0.4s ease 0s, box-shadow 0.4s ease 0s;"><small style="left: 0px; transition: background-color 0.4s ease 0s, left 0.2s ease 0s;"></small></span>
+                            <input data-url="<?php echo base_url("product/update_status/$item->id");?>" class="isActive"
+                                id="switch-2-2" type="checkbox" data-switchery data-color="#10c469"
+                                <?php echo ($item->durum) ? "checked" : "";  ?> />
 
                         </td>
                         <td>
-                            <a href="" class="btn btn-sm btn-danger btn-outline"><i class="fa fa-trash"></i>
+                            <a href="<?php echo base_url("product/delete/$item->id");?>"
+                                class="btn btn-sm btn-danger btn-outline"><i class="fa fa-trash"></i>
                                 Sil</a>
-                            <a href="" class="btn btn-sm btn-info btn-outline"><i class="fa fa-pencil-square-o"></i>
+                            <a href="<?php echo base_url("product/update_form/$item->id");?>"
+                                class="btn btn-sm btn-info btn-outline"><i class="fa fa-pencil-square-o"></i>
                                 Düzenle</a>
                         </td>
                     </tr>
-                                            <tr>
-                        <td>
-                            <div class="checkbox checkbox-primary">
-                                <input type="checkbox" id="checkbox-demo-1">
-                                <label for="checkbox-demo-1"></label>
-                            </div>
-                        </td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td>
-                            <input id="switch-2-2" type="checkbox" data-switchery="true" data-color="#10c469" style="display: none;"><span class="switchery switchery-default" style="box-shadow: rgb(223, 223, 223) 0px 0px 0px 0px inset; border-color: rgb(223, 223, 223); background-color: rgb(255, 255, 255); transition: border 0.4s ease 0s, box-shadow 0.4s ease 0s;"><small style="left: 0px; transition: background-color 0.4s ease 0s, left 0.2s ease 0s;"></small></span>
+                    <?php } ?> -->
 
-                        </td>
-                        <td>
-                            <a href="http://localhost/yonetim_paneli/panel/product/delete/36" class="btn btn-sm btn-danger btn-outline"><i class="fa fa-trash"></i>
-                                Sil</a>
-                            <a href="http://localhost/yonetim_paneli/panel/product/update_form/36" class="btn btn-sm btn-info btn-outline"><i class="fa fa-pencil-square-o"></i>
-                                Düzenle</a>
-                        </td>
-                    </tr>
-                                            <tr>
-                        <td>
-                            <div class="checkbox checkbox-primary">
-                                <input type="checkbox" id="checkbox-demo-1">
-                                <label for="checkbox-demo-1"></label>
-                            </div>
-                        </td>
-                        <td>7</td>
-                        <td>asdf</td>
-                        <td>12</td>
-                        <td>1253</td>
-                        <td>ednmedendmedm</td>
-                        <td>
-                            <input id="switch-2-2" type="checkbox" data-switchery="true" data-color="#10c469" checked="" style="display: none;"><span class="switchery" style="background-color: rgb(16, 196, 105); border-color: rgb(16, 196, 105); box-shadow: rgb(16, 196, 105) 0px 0px 0px 16px inset; transition: border 0.4s ease 0s, box-shadow 0.4s ease 0s, background-color 1.2s ease 0s;"><small style="left: 20px; background-color: rgb(255, 255, 255); transition: background-color 0.4s ease 0s, left 0.2s ease 0s;"></small></span>
-
-                        </td>
-                        <td>
-                            <a href="http://localhost/yonetim_paneli/panel/product/delete/31" class="btn btn-sm btn-danger btn-outline"><i class="fa fa-trash"></i>
-                                Sil</a>
-                            <a href="http://localhost/yonetim_paneli/panel/product/update_form/31" class="btn btn-sm btn-info btn-outline"><i class="fa fa-pencil-square-o"></i>
-                                Düzenle</a>
-                        </td>
-                    </tr>
-                                            <tr>
-                        <td>
-                            <div class="checkbox checkbox-primary">
-                                <input type="checkbox" id="checkbox-demo-1">
-                                <label for="checkbox-demo-1"></label>
-                            </div>
-                        </td>
-                        <td>3</td>
-                        <td>asdfsdaf</td>
-                        <td>12</td>
-                        <td>10</td>
-                        <td>asdfasdfgasdga</td>
-                        <td>
-                            <input id="switch-2-2" type="checkbox" data-switchery="true" data-color="#10c469" style="display: none;"><span class="switchery switchery-default" style="box-shadow: rgb(223, 223, 223) 0px 0px 0px 0px inset; border-color: rgb(223, 223, 223); background-color: rgb(255, 255, 255); transition: border 0.4s ease 0s, box-shadow 0.4s ease 0s;"><small style="left: 0px; transition: background-color 0.4s ease 0s, left 0.2s ease 0s;"></small></span>
-
-                        </td>
-                        <td>
-                            <a href="http://localhost/yonetim_paneli/panel/product/delete/32" class="btn btn-sm btn-danger btn-outline"><i class="fa fa-trash"></i>
-                                Sil</a>
-                            <a href="http://localhost/yonetim_paneli/panel/product/update_form/32" class="btn btn-sm btn-info btn-outline"><i class="fa fa-pencil-square-o"></i>
-                                Düzenle</a>
-                        </td>
-                    </tr>
-                                            <tr>
-                        <td>
-                            <div class="checkbox checkbox-primary">
-                                <input type="checkbox" id="checkbox-demo-1">
-                                <label for="checkbox-demo-1"></label>
-                            </div>
-                        </td>
-                        <td>5</td>
-                        <td>sdafdsafsdaf</td>
-                        <td>12</td>
-                        <td>231456456</td>
-                        <td>asdfasdfsadf</td>
-                        <td>
-                            <input id="switch-2-2" type="checkbox" data-switchery="true" data-color="#10c469" checked="" style="display: none;"><span class="switchery switchery-default" style="background-color: rgb(16, 196, 105); border-color: rgb(16, 196, 105); box-shadow: rgb(16, 196, 105) 0px 0px 0px 16px inset; transition: border 0.4s ease 0s, box-shadow 0.4s ease 0s, background-color 1.2s ease 0s;"><small style="left: 20px; background-color: rgb(255, 255, 255); transition: background-color 0.4s ease 0s, left 0.2s ease 0s;"></small></span>
-
-                        </td>
-                        <td>
-                            <a href="http://localhost/yonetim_paneli/panel/product/delete/33" class="btn btn-sm btn-danger btn-outline"><i class="fa fa-trash"></i>
-                                Sil</a>
-                            <a href="http://localhost/yonetim_paneli/panel/product/update_form/33" class="btn btn-sm btn-info btn-outline"><i class="fa fa-pencil-square-o"></i>
-                                Düzenle</a>
-                        </td>
-                    </tr>
-                    
 
                 </tbody>
 
             </table>
-            
+            <?php } ?>
+
         </div><!-- .widget -->
     </div><!-- END column -->
 </div>
--- panel/application/views/send_bulk_sms_v/update/content.php
+++ panel/application/views/send_bulk_sms_v/update/content.php
@@ -1,1 +1,36 @@
-Toplu SMS Gonder
+<div class="wrap">
+    <section class="app-content">
+        <div class="row">
+            <div class="col-md-12">
+                <h4 class="m-b-lg">
+                    Toplu SMS Gönder
+                </h4>
+
+               
+						<div class="col-12 grid-margin stretch-card">
+		<div class="card">
+			<div class="card-body">
+				<form class="forms-sample" method="post" action="../_class/yonetim_islem.php" enctype="multipart/form-data">
+					
+					<div class="form-group">
+						<label for="diger">Harici Telefon Numaraları <small class="text-danger">virgüllerle ayırınız</small></label>
+						<textarea id="diger" name="diger" class="form-control" rows="6"></textarea>
+					</div>
+					<div class="form-group">
+						<label for="aciklama">SMS İçeriği</label>
+						<textarea name="aciklama" rows="6" class="form-control" id="aciklama"></textarea>
+					</div>
+					<button type="submit" name="toplu_sms_gonder" class="btn btn-primary btn-icon-text btn-sm">
+						<i class="mdi mdi-file-check btn-icon-prepend"></i>
+						GÖNDER
+					</button>
+				</form>
+			</div>
+		</div>
+	</div>
+	
+						
+    <!-- .row -->
+                          
+    </section><!-- #dash-content -->
+</div>
