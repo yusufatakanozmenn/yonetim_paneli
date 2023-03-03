b51d9e96265b19cf264b5ff9f4013ac742f80b25
Yusuf Atakan Özmen <92385960+yusufatakanozmenn@users.noreply.github.com>
2023-03-03 17:53:54 +0300

    bayi sube

--- panel/application/views/dealer_branch_v/list/content.php
+++ panel/application/views/dealer_branch_v/list/content.php
@@ -49,6 +49,15 @@
                                         class="dataTables_wrapper container-fluid dt-bootstrap4 no-footer">
                                         <div class="row">
                                             <div class="col-sm-12">
+                                            <?php   
+                                            if (empty($items)) { ?>
+                                                <div class="alert alert-info text-center">
+                                                    <p>Burada herhangi bir veri bulunmamaktadır. Eklemek için lütfen <a
+                                                            href="<?php echo base_url("package/new_form"); ?>">tıklayınız</a>
+                                                    </p>
+                                                </div>
+                                                <?php } else { ?>
+
                                                 <table id="order-listingg"
                                                     class="table table-bordered table-hover dataTable no-footer"
                                                     role="grid" aria-describedby="order-listingg_info"
@@ -56,11 +65,10 @@
                                                     <thead class="headbg">
                                                         <tr role="row">
                                                             <th class="noshort sorting_disabled" style="width: 38.8px;"
-                                                                 data-placement="top"
-                                                                title="Tümünü Seç" rowspan="1" colspan="1" aria-label="
-									
-									
-								">
+                                                                data-placement="top" title="Tümünü Seç" rowspan="1"
+                                                                colspan="1" aria-label="">
+
+
                                                                 <input id="checkbox-4"
                                                                     class="select-all checkbox-custom" type="checkbox"
                                                                     style="width:100px;">
@@ -72,48 +80,43 @@
                                                                 aria-controls="order-listingg" rowspan="1" colspan="1"
                                                                 style="width: 959.8px;"
                                                                 aria-label="Başlık: artan sütun sıralamasını aktifleştir">
-                                                                Adı Soyadı</th>
+                                                                Şube / Firma Adı</th>
                                                             <th style="width: 69.8px;"
                                                                 class="secili text-center sorting" tabindex="0"
                                                                 aria-controls="order-listingg" rowspan="1" colspan="1"
                                                                 aria-label="Durum: artan sütun sıralamasını aktifleştir">
-                                                                Durum</th>
+                                                                Şehir /İlçe </th>
                                                             <th style="width: 114.6px;"
                                                                 class="text-center sorting_disabled" rowspan="1"
                                                                 colspan="1" aria-label="İşlem">İşlem</th>
                                                         </tr>
                                                     </thead>
-                                                    <tbody id="sortable" class="ui-sortable">
-                                                        <tr id="item-9" role="row" class="odd">
 
-                                                        <tr id="item-13" role="row" class="odd">
-                                                            <td>
-                                                                <div class="form-check mb-0 mt-0"><label
-                                                                        class="form-check-label"><input type="checkbox"
-                                                                            name="id[]" value="13"
-                                                                            class="form-check-input checkbox"><i
-                                                                            class="input-helper"></i></label></div>
-                                                            </td>
-                                                            <td class=" secili"><a href="belge-duzenle/13"
-                                                                    class="renk_baslik" title="Düzenle">ISO -
-                                                                    45001:2018</a></td>
-                                                            <td class=" secili text-center">
-                                                                <div class="badge badge-outline-success">Aktif</div>
-                                                            </td>
-                                                            <td>
-                                                               
-                                                                <a href=""
-                                                                    class="btn btn-sm btn-info btn-outline"><i
-                                                                        class="fa fa-pencil-square-o"></i>
-                                                                    </a> 
-                                                                    <a href=""
-                                                                    class="btn btn-sm btn-danger btn-outline"><i
-                                                                        class="fa fa-trash"></i>
-                                                                    </a>
-                                                            </td>
+                                                    <tbody id="sortable" class="ui-sortable">
+                                                        <?php foreach ($items as $item) { ?>
+                                                        <td>
+                                                            <div class="form-check mb-0 mt-0"><label
+                                                                    class="form-check-label"><input type="checkbox"
+                                                                        name="id[]" value="13"
+                                                                        class="form-check-input checkbox"><i
+                                                                        class="input-helper"></i></label></div>
+                                                        </td>
+                                                        <td class=" secili"><?php echo $item->adi;?></td>
+                                                        <td class=" secili text-center"><?php echo $item->adres;?></td>
+                                                        <td>
+
+                                                            <a href="" class="btn btn-sm btn-info btn-outline"><i
+                                                                    class="fa fa-pencil-square-o"></i>
+                                                            </a>
+                                                            <a href="" class="btn btn-sm btn-danger btn-outline"><i
+                                                                    class="fa fa-trash"></i>
+                                                            </a>
+                                                        </td>
                                                         </tr>
+                                                        <?php } ?>
                                                     </tbody>
                                                 </table>
+                                                <?php } ?>
                                                 <div id="order-listingg_processing" class="dataTables_processing card"
                                                     style="display: none;">İşleniyor...</div>
                                             </div>
