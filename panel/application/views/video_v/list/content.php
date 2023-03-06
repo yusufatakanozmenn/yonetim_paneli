966cf10da6dd0047a3a9b5032772bd214eec138a
Yusuf Atakan Özmen <92385960+yusufatakanozmenn@users.noreply.github.com>
2023-03-06 11:48:43 +0300

    Update content.php

--- panel/application/views/video_v/list/content.php
+++ panel/application/views/video_v/list/content.php
@@ -63,36 +63,9 @@
                                 </td>
                             </tr>
                         </tbody>
-
                     <?php } ?>
                 </table>
-
-            <?php } ?>
-            <div class="row">
-                <div class="col-sm-12 col-md-5">
-                    <div class="dataTables_info" id="order-listingg_info" role="status" aria-live="polite">5 kayıttan 1
-                        - 5 arasındaki kayıtlar gösteriliyor
-                    </div>
-
-                    <div class="col-sm-12 col-md-7">
-                        <div class="dataTables_paginate paging_simple_numbers" id="order-listingg_paginate">
-                            <ul class="pagination">
-                                <li class="paginate_button page-item previous disabled" id="order-listingg_previous"><a
-                                        href="#" aria-controls="order-listingg" data-dt-idx="0" tabindex="0"
-                                        class="page-link">Önceki</a></li>
-                                <li class="paginate_button page-item active"><a href="#" aria-controls="order-listingg"
-                                        data-dt-idx="1" tabindex="0" class="page-link">1</a></li>
-                                <li class="paginate_button page-item next disabled" id="order-listingg_next"><a href="#"
-                                        aria-controls="order-listingg" data-dt-idx="2" tabindex="0"
-                                        class="page-link">Sonraki</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
+            <?php } ?>          
         </div><!-- .widget -->
-
     </div><!-- END column -->
 </div>
