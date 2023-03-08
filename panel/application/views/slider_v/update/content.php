d47917d31d3fb4a4a7dbfc6b77534a8b4c2d9f29
Yusuf Atakan Özmen <92385960+yusufatakanozmenn@users.noreply.github.com>
2023-03-08 16:04:55 +0300

    slider

--- panel/application/views/slider_v/update/content.php
+++ panel/application/views/slider_v/update/content.php
@@ -15,21 +15,17 @@
                     </div>
                     <div class="form-group">
                         <label for="adi">Slayt Başlığı</label>
-                        <input type="text" class="form-control form-control-sm" name="adi" id="adi" value="<?=$item->adi?>">
+                        <input type="text" class="form-control form-control-sm" name="adi" id="adi"
+                            value="<?=$item->adi?>">
                     </div>
                     <div class="form-group">
                         <label for="url">Slayt URL</label>
-                        <input type="text" class="form-control form-control-sm" name="url" id="url" value="<?=$item->url?>">
+                        <input type="text" class="form-control form-control-sm" name="url" id="url"
+                            value="<?=$item->url?>">
                     </div>
-                    <div class="form-group">
-                        <div class="form-check">
-                            <label class="form-check-label">
-                                <input type="checkbox" name="sekme" class="form-check-input">Tıklandığında yeni sekmeye
-                                gitsin mi ?<i class="input-helper"></i><i class="input-helper"></i></label>
-                        </div>
-                    </div>
+
                     <div class="form-group row col-md-6">
-                        <label>Slayt Görseli Seçiniz (Önerilen Ölçüler: 1920x760)</label>                       
+                        <label>Slayt Görseli Seçiniz (Önerilen Ölçüler: 1920x760)</label>
                         <div class="input-group col-xs-12">
                             <div class="form-group">
                                 <input type="file" name="firma_footerlogo" class="file-upload-default">
@@ -43,14 +39,7 @@
                         </div>
                     </div>
             </div>
-            <div class="form-group">
-                <label class="d-block" for="durum">Durum</label>
-                <label class="switch">
-                    <input type="checkbox" name="durum" id="durum" value="<?=$item->durum?>" checked="">
-                    <span class="slider"></span>
-                </label>
 
-            </div>
             <div class="form-group">
                 <label for="adi">Açıklama</label>
                 <textarea class="form-control" name="aciklama"><?=$item->aciklama?></textarea>
@@ -59,14 +48,30 @@
             <div class="form-group">
                 <label for="video">Varsa Video Embed Video ID <a href="images/embed.png" target="_blank"
                         class="hoverZoomLink">Neresi ?</a></label>
-                <input type="text" class="form-control form-control-sm" name="video" id="video" value="<?=$item->video?>">
+                <input type="text" class="form-control form-control-sm" name="video" id="video"
+                    value="<?=$item->video?>">
+            </div>
+            <div class="form-group">
+                <div class="form-check">
+                    <label class="form-check-label">
+                        <input type="checkbox" name="sekme" class="form-check-input">Tıklandığında yeni sekmeye
+                        gitsin mi ?<i class="input-helper"></i><i class="input-helper"></i></label>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="d-block" for="durum">Durum</label>
+                <label class="switch">
+                    <input type="checkbox" name="durum" id="durum" value="<?=$item->durum?>" checked="">
+                    <span class="slider"></span>
+                </label>
+
             </div>
 
             <div class="form-group">
-            <button type="submit" class="btn btn-success btn-icon-text btn-sm"> GÖNDER </button>
-            <a href="<?php echo base_url("slider"); ?>"></a>
-            </form>
-        </div>
+                <button type="submit" class="btn btn-success btn-icon-text btn-sm"> GÖNDER </button>
+                <a href="<?php echo base_url("slider"); ?>"></a>
+                </form>
+            </div>
 
             </form>
         </div>
