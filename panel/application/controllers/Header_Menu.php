eed44b9fd1b0b78f299ba13dcf2afa62c7287f58
Yusuf Atakan Özmen <92385960+yusufatakanozmenn@users.noreply.github.com>
2023-03-15 12:39:03 +0300

    header menu

--- panel/application/controllers/Header_Menu.php
+++ panel/application/controllers/Header_Menu.php
@@ -9,13 +9,92 @@ class Header_Menu extends CI_Controller {
 			redirect(base_url("login"));
 		}
 		$this->viewFolder = "header_menu_v";
+		$this->load->model("header_menu_model");
 	}
     public function index(){
         $viewData = new stdClass();
+		$items = $this->header_menu_model->get_all(
+			array()
+			
+		);
         $viewData->viewFolder=$this->viewFolder;
-        $viewData->subViewFolder="update";
-
+        $viewData->subViewFolder="list";
+		$viewData->items=$items;
         $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index",$viewData);
+    }
+
+	public function new_form(){
+		$viewData = new stdClass();
+		$viewData->viewFolder=$this->viewFolder;
+		$viewData->subViewFolder="add";
+		$this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index",$viewData);
+	}
+
+	public function update_form($id){
+        $viewData = new stdClass();
+        $item = $this->header_menu_model->get(
+            array(
+                "id" => $id
+            )
+        );
+        $viewData->viewFolder = $this->viewFolder;
+        $viewData->subViewFolder = "update";
+        $viewData->item = $item;
+        $this->load->view("{$this->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+    }
+
+	public function update_status($id){
+        if($id){
+            $isActive = ($this->input->post("data") === "true") ? 1 : 0 ;
+            $insert = $this->header_menu_model->update(
+                array(
+                    "id" => $id
+                ),
+                array(
+                    "durum" => $isActive            
+                )
+                );
+        }else{
+            echo 'Hatali islem';
+        }
+    }
+
+	public function delete($id){
+        $delete = $this->header_menu_model->delete(
+            array(
+                "id" => $id
+            )
+        );
+        if($delete){
+            redirect(base_url("header_menu"));
+        }
+        else{
+            echo "Silme İşlemi Gerçekleşmedi";
+        }
+    }
+    public function rankSetter(){
+
+
+        $data = $this->input->post("data");
+
+        parse_str($data, $order);
+
+        $items = $order["ord"];
+
+        foreach ($items as $rank => $id){
+
+            $this->header_menu_model->update(
+                array(
+                    "id"        => $id,
+                    "rank !="   => $rank
+                ),
+                array(
+                    "rank"      => $rank
+                )
+            );
+
+        }
+
     }
 
 
---
+++ panel/application/views/header_menu_v/list/content.php
@@ -0,0 +1,67 @@
+<div class="row">
+    <div class="col-md-12">
+        <h4 class="m-b-lg">
+            Menü Listesi
+            <a href="<?php echo base_url("header_menu/new_form"); ?>" class="btn btn-outline btn-primary btn-xs pull-right"> <i class="fa fa-plus"></i> Yeni Ekle</a>
+        </h4>
+    </div><!-- END column -->
+    <div class="col-md-12">
+        <div class="widget p-lg">
+
+            <?php if(empty($items)) { ?>
+
+                <div class="alert alert-info text-center">
+                    <p>Burada herhangi bir veri bulunmamaktadır. Eklemek için lütfen <a href="<?php echo base_url("header_menu/new_form"); ?>">tıklayınız</a></p>
+                </div>
+
+            <?php } else { ?>
+
+                <table class="table table-hover table-striped">
+                    <thead>
+                        <th><i class="fa fa-reorder"></i></th>
+                        <th>#id</th>
+                        <th>Menu Başlık</th>
+                        <th>Menu Url</th>
+                        <th>Durumu</th>
+                        <th>İşlem</th>
+                    </thead>
+                    <tbody class="sortable" data-url="<?php echo base_url("header_menu/rankSetter"); ?>">
+
+                        <?php foreach($items as $item) { ?>
+
+                            <tr id="ord-<?php echo $item->id; ?>">
+                                <td><i class="fa fa-reorder"></i></td>
+                                <td>#<?php echo $item->id; ?></td>
+                                <td><?php echo $item->menu_isim; ?></td>
+                                <td><?php echo $item->menu_url; ?></td>
+                                <td>
+                                    <input
+                                        data-url="<?php echo base_url("header_menu/update_status/$item->id"); ?>"
+                                        class="isActive"
+                                        type="checkbox"
+                                        data-switchery
+                                        data-color="#10c469"
+                                        <?php echo ($item->menu_durum) ? "checked" : ""; ?>
+                                    />
+                                </td>
+                                <td>
+                                    <button
+                                        data-url="<?php echo base_url("header_menu/delete/$item->id"); ?>"
+                                        class="btn btn-sm btn-danger btn-outline remove-btn">
+                                        <i class="fa fa-trash"></i> Sil
+                                    </button>
+                                    <a href="<?php echo base_url("header_menu/update_form/$item->id"); ?>" class="btn btn-sm btn-info btn-outline"><i class="fa fa-pencil-square-o"></i> Düzenle</a>
+                                </td>
+                            </tr>
+
+                        <?php } ?>
+
+                    </tbody>
+
+                </table>
+
+            <?php } ?>
+
+        </div><!-- .widget -->
+    </div><!-- END column -->
+</div>
